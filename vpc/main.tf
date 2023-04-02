3a72363d2c8cca64d6597abb66358313aac8ee21
Farsin Ahammed <56280489+farsin28@users.noreply.github.com>
2023-04-03 03:04:47 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,36 +1,26 @@
-
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "ap-south-1"
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
+  region = "ap-south-1"
 }
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+
+resource "aws_vpc" "farsin_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "farsin_sub" {
+  vpc_id = aws_vpc.farsin_vpc.id
   cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
+  availability_zone = "ap-south-1a"
 }
-resource "aws_network_interface" "mynic" {
-  subnet_id       = aws_subnet.main.id
-  private_ips     = ["10.0.1.1"]
 
+resource "aws_network_interface" "farsin_nic" {
+  subnet_id = aws_subnet.farsin_sub.id
 }
-resource "aws_instance" "farsin_web" {
-  ami           = "ami-0fdea1353c525c182" # mumbai AZ ap-south-1
+
+resource "aws_instance" "farsin_ec2" {
+  ami           = "ami-0fdea1353c525c182" # ap-south-1
   instance_type = "t2.micro"
-
   network_interface {
-    network_interface_id = aws_network_interface.mynic.id
-    device_index         = 0
+    device_index = 0
+    network_interface_id = aws_network_interface.farsin_nic.id
   }
-
+}
